94c7f4dc017a19e031b49b349c33594fcced29f1
Franco <fjimenez@mobydigital.com>
2025-11-12 12:41:47 -0300

    feat: add custom exceptions for better error handling

---
+++ src/main/java/com/microservicios/tasks/exception/InvalidTokenException.java
@@ -0,0 +1,11 @@
+package com.microservicios.tasks.exception;
+
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+
+    public InvalidTokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
