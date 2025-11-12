f53dbbe235717a80e318cc0ce8ba89d74b180883
Franco <fjimenez@mobydigital.com>
2025-11-12 12:05:02 -0300

    refactor: migrate TaskListDto to Lombok and add validations

--- src/main/java/com/microservicios/tasks/dto/TaskListDto.java
+++ src/main/java/com/microservicios/tasks/dto/TaskListDto.java
@@ -1,51 +1,26 @@
 package com.microservicios.tasks.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskListDto {
     private String id;
+
+    @NotBlank(message = "Title is required")
     private String title;
-    private String type; // "tasklist"
-    private List<TaskDto> tasks;
 
-    public TaskListDto() {}
-
-    public TaskListDto(String id, String title, String type) {
-        this.id = id;
-        this.title = title;
-        this.type = type;
-    }
-
-    // Getters and Setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public List<TaskDto> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(List<TaskDto> tasks) {
-        this.tasks = tasks;
-    }
+    @NotNull(message = "Tasks list cannot be null")
+    @Builder.Default
+    private List<TaskDto> tasks = new ArrayList<>();
 }
